74d9f22362b511a19db95856b2cea8c6fe9f2f9e
taruneluri <taruneluri2106@gmail.com>
2022-07-18 12:54:37 +0530

    task 2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Header from './components/header';
+import Section1 from './components/section1';
+import Features from './components/features';
+import Jobori from './components/jobori';
+import Busori from './components/busori';
+import Lancou from './components/lancou';
+import Fit from './components/fit';
+import Stutest from './components/stutest';
+import Footer from './components/footer';
+import Tutorial from './components/tutorials';
+function App() {
+  return (
+    <div className='App'>
+      <Header></Header>
+      <Section1></Section1>
+      <Features></Features>
+      <Jobori></Jobori>
+      <Busori></Busori>
+      <Tutorial></Tutorial>
+      <Lancou></Lancou>
+      <Fit></Fit>
+      <Stutest></Stutest>
+      <Footer></Footer>
+    </div>
+    
+  );
+}
+
+export default App;
